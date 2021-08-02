cbb8b6b8d7aee9ee933ce4e49502331130d4971f
macmc <macmecy@gmail.com>
2021-08-02 18:43:53 +0200

    list for advance search interface

--- js/interface.js
+++ js/interface.js
@@ -20,3 +20,69 @@ dropdown.forEach(
     }),
   false
 )
+
+// create ingredient list
+const ingredients = []
+const appliance = []
+const ustensils = []
+
+recipes.forEach((e) => {
+  e.ingredients.forEach((i) => {
+    if (!ingredients.includes(i.ingredient)) {
+      ingredients.push(i.ingredient)
+    }
+  })
+  e.ustensils.forEach((u) => {
+    if (!ustensils.includes(u)) {
+      ustensils.push(u)
+    }
+  })
+  if (!appliance.includes(e.appliance)) {
+    appliance.push(e.appliance)
+  }
+})
+
+console.log(appliance)
+console.log(ustensils)
+
+// create ul of ingredient
+
+const listIngredient = `
+<ul class="my-list">
+${ingredients
+  .map(
+    (e) =>
+      `<li class="list-group-item w-100 border-0 bg-transparent"><a class="dropdown-item" href="#">${e}</a></li>`
+  )
+  .join('')}
+</ul>
+`
+
+const listApp = `
+<ul class="my-list">
+${appliance
+  .map(
+    (e) =>
+      `<li class="list-group-item w-100 border-0 bg-transparent"><a class="dropdown-item" href="#">${e}</a></li>`
+  )
+  .join('')}
+</ul>
+`
+
+const listUsten = `
+<ul class="my-list">
+${ustensils
+  .map(
+    (e) =>
+      `<li class="list-group-item w-100 border-0 bg-transparent"><a class="dropdown-item" href="#">${e}</a></li>`
+  )
+  .join('')}
+</ul>
+`
+const dropIngre = document.querySelector('#ingredients')
+const dropApp = document.querySelector('#appliance')
+const dropUsten = document.querySelector('#ustensils')
+
+dropIngre.innerHTML = listIngredient
+dropApp.innerHTML = listApp
+dropUsten.innerHTML = listUsten
