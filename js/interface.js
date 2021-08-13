03d5b4ccfd4d88fb201deff0ac9eaefcb5a72ed5
macmc <macmecy@gmail.com>
2021-08-13 15:00:07 +0200

    all search algos finishes

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,5 @@
 const dropdown = document.querySelectorAll('#dropdownMenuButton')
-let tab = []
+const tab = [[], [], []]
 /*
  * First search implementation - the simple
  */
@@ -185,7 +185,7 @@ function createList(list) {
   for (let i = 0; i < list.length; i += 3) {
     let t = `<ul class="list-group list-group-horizontal">`
     for (j; j <= pointer; j += 1) {
-      t += `<li class="list-group-item w-100 border-0 bg-transparent .text-truncate"><a class="dropdown-item" href="#" title="${list[j]}"><span class="d-inline-block text-truncate" style="width: 10rem;">${list[j]}</span></a></li>`
+      t += `<li class="list-group-item w-100 border-0 bg-transparent"><a href="#" class="dropdown-item" title="${list[j]}"><span class="d-inline-block text-truncate" style="width: 10rem;">${list[j]}</span></a></li>`
     }
     t += `</ul>`
     pointer += 3
@@ -198,85 +198,144 @@ function createList(list) {
 }
 
 // Select filter tags
-const itemsIngredients = document.querySelector('#ingredients')
+const items = document.querySelector('#aSearch')
 
-function searchTag(type, tags) {
-  const nb = tags.length
+function searchTag(tags) {
+  const nbi = tags[0].length
+  const nba = tags[1].length
+  const nbu = tags[2].length
+  const nbt = nbi + nba + nbu
 
-  if (type === 'ingredients') {
-    return recipes.filter((el) => {
-      let nbFind = 0
+  return recipes.filter((el) => {
+    let nbFind = 0
+    if (nbi > 0) {
       el.ingredients.forEach((i) => {
-        if (tags.includes(i.ingredient)) {
+        if (tags[0].includes(i.ingredient.toLowerCase())) {
           nbFind += 1
         }
       })
-      if (nb === nbFind) {
-        return true
+    }
+    if (nba > 0) {
+      if (tags[1].includes(el.appliance.toLowerCase())) {
+        nbFind += 1
       }
-    })
-  }
+    }
+    if (nbu > 0) {
+      el.ustensils.forEach((u) => {
+        if (tags[2].includes(u.toLowerCase())) {
+          nbFind += 1
+        }
+      })
+    }
+
+    if (nbt === nbFind) {
+      return true
+    }
+  })
 }
 
-itemsIngredients.addEventListener('click', (i) => {
+items.addEventListener('click', (i) => {
   if (i.target) {
     if (i.target.nodeName === 'SPAN' && i.target.textContent) {
-      tab.push(i.target.textContent)
-      console.log(tab)
-      generateList(searchTag('ingredients', tab))
+      greatParentId =
+        i.target.parentElement.parentElement.parentElement.parentElement.id
+
+      if (greatParentId === 'ingredients') {
+        tab[0].push(i.target.textContent.toLowerCase())
+      } else if (greatParentId === 'appliances') {
+        tab[1].push(i.target.textContent.toLowerCase())
+      } else {
+        tab[2].push(i.target.textContent.toLowerCase())
+      }
+
+      generateList(searchTag(tab))
       document.querySelector('#forTags').innerHTML += `
-    <a class="btn btn-primary my-btn--tag text-center">
+    <a class="btn btn-primary my-btn--tag text-center" type="${greatParentId}">
       <span class="text-nowrap">${i.target.textContent}</span>
-      <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none"
-        xmlns="http://www.w3.org/2000/svg">
-        <path
-            d="M12.59 6L10 8.59L7.41 6L6 7.41L8.59 10L6 12.59L7.41 14L10 11.41L12.59 14L14 12.59L11.41 10L14 7.41L12.59 6ZM10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18Z"
-            fill="white" />
-      </svg>
+      <span id="tag" class="flex-shrink-0">
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <path
+              d="M12.59 6L10 8.59L7.41 6L6 7.41L8.59 10L6 12.59L7.41 14L10 11.41L12.59 14L14 12.59L11.41 10L14 7.41L12.59 6ZM10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18Z"
+              fill="white" />
+        </svg>
+      </span>
     </a>
     `
     }
   }
 })
 
-/* items.forEach((i) =>
-  i.addEventListener('click', () => {
-    tab.push(i.innerText)
-    console.log(i.innerText)
-    console.log(tab)
-    generateList(searchTag('ingredients', tab))
-    document.querySelector('#forTags').innerHTML += `
-  <a class="btn btn-primary my-btn--tag text-center">
-    <span class="text-nowrap">${i.innerText}</span>
-    <svg class="flex-shrink-0" width="20" height="20" viewBox="0 0 20 20" fill="none"
-      xmlns="http://www.w3.org/2000/svg">
-      <path
-          d="M12.59 6L10 8.59L7.41 6L6 7.41L8.59 10L6 12.59L7.41 14L10 11.41L12.59 14L14 12.59L11.41 10L14 7.41L12.59 6ZM10 0C4.47 0 0 4.47 0 10C0 15.53 4.47 20 10 20C15.53 20 20 15.53 20 10C20 4.47 15.53 0 10 0ZM10 18C5.59 18 2 14.41 2 10C2 5.59 5.59 2 10 2C14.41 2 18 5.59 18 10C18 14.41 14.41 18 10 18Z"
-          fill="white" />
-    </svg>
-  </a>
-  `
-  })
-) */
+// filter les tags
+items.addEventListener('input', (i) => {
+  if (i.target.value.length > 2) {
+    const filter = i.target.value.toLowerCase()
+    let targetList
+    let selector
 
-// Close filters tags
-const listTags = document.querySelector('#forTags')
+    if (i.target.id === 'ing') {
+      targetList = allIngredients
+      selector = 'ingredients'
+    }
+    if (i.target.id === 'app') {
+      targetList = allAppliances
+      selector = 'appliances'
+    }
+    if (i.target.id === 'ust') {
+      targetList = allUstensils
+      selector = 'ustensils'
+    }
 
-listTags.addEventListener('click', (e) => {
-  e.stopPropagation()
-  if (e.target.nodeName === 'path') {
-    tab = tab.filter(function (i) {
-      return (
-        i !== e.target.parentNode.parentNode.querySelector('span').textContent
-      )
+    targetList = targetList.filter((el) => {
+      if (el.toLowerCase().includes(filter)) {
+        return true
+      }
     })
-    generateList(searchTag('ingredients', tab))
-    e.target.parentNode.parentNode.remove()
 
-    /* if (e.target.nodeName === 'svg') {
-      e.target.parentNode.remove()
-    } else if (e.target.nodeName === 'path') {
-      e.target.parentNode.parentNode.remove()
-    } */
+    document.querySelector(`#${selector}`).innerHTML = createList(targetList)
+  } else if (i.target.value.length === 2) {
+    setTimeout(() => {
+      document.querySelector('#ingredients').innerHTML =
+        createList(allIngredients)
+      document.querySelector('#appliances').innerHTML =
+        createList(allAppliances)
+      document.querySelector('#ustensils').innerHTML = createList(allUstensils)
+    }, 250)
   }
+
+  // Close filters tags
+  const listTags = document.querySelector('#forTags')
+
+  listTags.addEventListener('click', (e) => {
+    e.stopPropagation()
+    let parent
+    if (e.target.nodeName === 'svg') {
+      parent = e.target.parentElement.parentElement
+    } else if (e.target.nodeName === 'path') {
+      parent = e.target.parentElement.parentElement.parentElement
+    }
+
+    if (parent !== undefined) {
+      let index
+
+      if (parent.getAttribute('type') === 'ingredients') {
+        index = 0
+      }
+      if (parent.getAttribute('type') === 'appliances') {
+        index = 1
+      }
+      if (parent.getAttribute('type') === 'ustensils') {
+        index = 2
+      }
+      const tabIndex = tab[index].indexOf(
+        parent.querySelector('span').textContent.toLowerCase()
+      )
+      if (tabIndex !== -1) {
+        tab[index].splice(tabIndex, 1)
+      }
+
+      generateList(searchTag(tab))
+      parent.remove()
+    }
+  })
 })
